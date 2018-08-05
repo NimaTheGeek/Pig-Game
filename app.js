042c6bb57a0f022a1261fa0f2622656f451cbb2b
Nima M.Farahani <nima_f101@yahoo.com>
2018-08-05 17:27:45 -0400

    added main functionality

--- app.js
+++ app.js
@@ -8,3 +8,46 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores = [0,0];
+var roundScores = 0;
+var activePlayer = 0;
+
+//hide the selection
+document.querySelector('.dice').style.display = 'none' ;
+
+//EventListener take 2 par, first the event then the action performed
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    var dice = Math.floor(Math.random() * 6) + 1;
+    var diceDOM = document.querySelector('.dice');
+    
+    //display the selection
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+    if( dice !== 1 ){
+        
+        roundScores += dice;
+        // id is written this way so the content can be updated
+        // based on the payer, playing 
+        document.getElementById('current-' + activePlayer).innerHTML = '<em>' + roundScores + '</em>';
+    } else{
+        
+        roundScores = 0;
+        dice = 0;
+        document.getElementById('current-' + activePlayer).innerHTML = '<em>' + roundScores + '</em>';
+        
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        
+        //toggle active user graphic when user changes 
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        roundScores += dice;
+
+    }
+    
+});
+
+
+
+
